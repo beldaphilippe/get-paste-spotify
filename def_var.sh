ba5191421ef7c59ad8e90411baa4ac87cd27e815
beldaphilippe <belda.philippe@proton.me>
2024-10-21 19:16:59 +0200

    Create def_var.sh

---
+++ def_var.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env /bin/sh
+export CLIENT_ID_DEST=""
+export CLIENT_SECRET_DEST=""
+
+export CLIENT_ID_SRC=""
+export CLIENT_SECRET_SRC=""
+
+export REDIRECT_URI="http://localhost:8888/callback"
