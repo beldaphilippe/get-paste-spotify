688a80f0fabad760b098d87c8728ff975891350b
Philippe Belda <beldap@ensipc85.ensimag.fr>
2024-10-22 16:14:22 +0200

    update 1

--- get_liked_songs.py
+++ get_liked_songs.py
@@ -1,34 +1,69 @@
+""" put user's liked songs and followed artists"""
+
 import spotipy
 import os
 import json
 from spotipy.oauth2 import SpotifyOAuth
 
-client_id = os.environ.get('CLIENT_ID_SRC')
-client_secret = os.environ.get('CLIENT_SECRET_SRC')
-redirect_uri = os.environ.get('REDIRECT_URI')
 
-scope = 'user-library-read'
+def spotify_authenticate():
+    """ Spotify authentication with spotipy """
+    client_id = os.environ.get('CLIENT_ID_SRC')
+    client_secret = os.environ.get('CLIENT_SECRET_SRC')
+    redirect_uri = os.environ.get('REDIRECT_URI')
 
-# Authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri,
-                                               scope=scope))
+    scope = 'user-library-read'
 
-current_user = sp.current_user()
-print(f"Utilisateur connecté : {current_user['display_name']}")
+    # Authentication
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                                   client_secret=client_secret,
+                                                   redirect_uri=redirect_uri,
+                                                   scope=scope))
 
-#get the songs
-NB_LIKED_SONGS = 1000
-response = {}
-i=0
-while i<NB_LIKED_SONGS:
-    results = sp.current_user_saved_tracks(offset=i, limit=50)
-    response[i] = results
-    i+=50
+    current_user = sp.current_user()
+    print(f"Utilisateur connecté : {current_user['display_name']}")
+    return sp
 
-#dump to json
-with open('spotify_data.json', 'w') as fp:
-    json.dump(response, fp)
 
-os.remove(".cache")
+def get_liked_songs():
+    """ Get the liked songs """
+    MAX_LIKED_SONGS = 8000
+    response = []
+    i=0
+    while i<MAX_LIKED_SONGS:
+        results = sp.current_user_saved_tracks(offset=i, limit=50)
+        if results == []:
+            break
+        response += results
+        i += 50
+    return response
+
+
+def get_followed_artists():
+    """ Get the followed artists """
+    MAX_FOLLOWED_ARTISTS = 8000
+    response = []
+    i=0
+    while i<MAX_FOLLOWED_ARTISTS:
+        results = sp.current_user_followed_artists(offset=i, limit=20)
+        if results == []:
+            break
+        response += results
+        i += 20
+    return response
+
+
+def main():
+    spotify_authenticate()
+    data = {}
+    data["liked_songs"] = get_liked_songs()
+    data["followed_artists"] = get_followed_artists()
+
+    # Dump to json
+    with open('spotify_data.json', 'w') as fp:
+        json.dump(data, fp)
+
+    os.remove(".cache")
+
+
+main()
