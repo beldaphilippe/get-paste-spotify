9ece1ad11394104cd13d65047deef019932007bd
Philippe <belda.philippe@proton.me>
2024-11-05 22:15:38 +0100

    update

--- get_infos.py
+++ get_infos.py
@@ -12,25 +12,26 @@ def spotify_authenticate():
     client_secret = os.environ.get('CLIENT_SECRET_SRC')
     redirect_uri = os.environ.get('REDIRECT_URI')
 
-    scope = 'user-library-read'
+    scope = 'user-library-read, user-follow-read'
 
     # Authentication
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                    client_secret=client_secret,
                                                    redirect_uri=redirect_uri,
-                                                   scope=scope))
+                                                   scope=scope,
+                                                   cache_path=None))
 
     current_user = sp.current_user()
     print(f"Utilisateur connecté : {current_user['display_name']}")
     return sp
 
 
-def get_liked_songs():
+def get_liked_songs(sp):
     """ Returns the list of uris the liked songs """
     results_uris = []
     i = 0
     results = sp.current_user_saved_tracks(offset=i)
-    while results != []:
+    while results['items'] != []:
         for item in results['items']:
             results_uris.append(item['track']['uri'])
         i += 50
@@ -38,30 +39,45 @@ def get_liked_songs():
     return results_uris
 
 
-def get_followed_artists():
-    """ Get the followed artists """
-    artists_ids = []
-    i = 0
-    results = sp.current_user_followed_artists(offset=i)
-    while results != []:
-        for item in results['item']:
-            artists_ids.append(item['artist']['id'])
-        i += 20
-        results = sp.current_user_followed_artists(offset=i)
-    return response
+# def get_followed_artists(sp):
+    # """ Get the followed artists """
+    # after = None
+    # artists_ids = []
+    # results = sp.current_user_followed_artists(after=after)
+    # while results['artists']['items'] != []:
+        # print('a')
+        # for item in results["artists"]["items"]:
+            # artists_ids.append(item['id'])
+        # after = results['artists']['cursors']['after']
+        # results = sp.current_user_followed_artists(after=after)
+    # return response
 
+
+def get_followed_artists(sp):
+    """ Get the followed artists """
+    limit = 20
+    after = None
+    artists_ids = []
+    while True:
+        response = sp.current_user_followed_artists(limit=limit, after=after)
+        for item in response["artists"]["items"]:
+            artists_ids.append(item['id'])
+        if not response['artists']['cursors']['after']:
+            break
+        after = response['artists']['cursors']['after']
+    return artists_ids
 
 def main():
-    spotify_authenticate()
+    sp = spotify_authenticate()
     data = {}
-    data["liked_songs"] = get_liked_songs()
-    data["followed_artists"] = get_followed_artists()
+    data["liked_songs"] = get_liked_songs(sp)
+    data["followed_artists"] = get_followed_artists(sp)
 
     # Dump to json
     with open('spotify_data.json', 'w') as fp:
         json.dump(data, fp)
 
-    os.remove(".cache")
+    # os.remove(".cache")
 
 
 main()
--- paste_infos.py
+++ paste_infos.py
@@ -12,24 +12,25 @@ SONGS_FILE = "spotify_data.json"
 
 def spotify_authenticate():
     """ Spotify authentication with spotipy """
-    client_id = os.environ.get('CLIENT_ID_SRC')
-    client_secret = os.environ.get('CLIENT_SECRET_SRC')
+    client_id = os.environ.get('CLIENT_ID_DEST')
+    client_secret = os.environ.get('CLIENT_SECRET_DEST')
     redirect_uri = os.environ.get('REDIRECT_URI')
 
-    scope = 'user-library-read'
+    scope = 'user-library-modify, user-follow-modify'
 
     # Authentication
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                    client_secret=client_secret,
                                                    redirect_uri=redirect_uri,
-                                                   scope=scope))
+                                                   scope=scope,
+                                                   cache_path=None))
 
     current_user = sp.current_user()
     print(f"Utilisateur connecté : {current_user['display_name']}")
     return sp
 
 
-def paste_liked_songs(uris):
+def paste_liked_songs(sp, uris):
     i = 0
     increment = 30
     while i+increment <= len(uris):
@@ -42,12 +43,12 @@ def paste_liked_songs(uris):
         i += increment
 
 
-def follow_artists(ids):
+def follow_artists(sp, ids):
     i = 0
     increment = 30
     while i+increment <= len(ids):
         try:
-            response = sp.user_follow_artist(ids[i: i+increment])
+            response = sp.user_follow_artists(ids[i: i+increment])
             if response != None:
                 print(f"Réponse de l'API : {response}")
         except spotipy.exceptions.SpotifyException:
@@ -56,13 +57,16 @@ def follow_artists(ids):
 
 
 def main():
-    spotify_authenticate()
+    sp = spotify_authenticate()
     with open(SONGS_FILE, 'r') as fp:
         response = json.load(fp)
     songs = response['liked_songs']
     artists = response['followed_artists']
-    
-    paste_liked_songs(songs)
-    follow_artists(artists)
+
+    paste_liked_songs(sp, songs)
+    follow_artists(sp, artists)
 
     os.remove(".cache")
+
+
+main()
